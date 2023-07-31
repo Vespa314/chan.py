b0ed4fb972eb85056c45201b203d04786a7ee2f2
Vespa314 <kylensherlock@gmail.com>
2023-07-31 17:23:31 +0800

    增加特征配置

--- BuySellPoint/BS_Point.py
+++ BuySellPoint/BS_Point.py
@@ -1,6 +1,7 @@
-from typing import Generic, List, Optional, TypeVar
+from typing import Dict, Generic, List, Optional, TypeVar, Union
 
 from Bi.Bi import CBi
+from ChanModel.Features import CFeatures
 from Common.CEnum import BSP_TYPE
 from Seg.Seg import CSeg
 
@@ -16,6 +17,7 @@ class CBS_Point(Generic[LINE_TYPE]):
         self.relate_bsp1 = relate_bsp1
 
         self.bi.bsp = self  # type: ignore
+        self.features = CFeatures()
 
         self.is_segbsp = False
 
@@ -31,3 +33,6 @@ class CBS_Point(Generic[LINE_TYPE]):
             self.relate_bsp1 = relate_bsp1
         elif relate_bsp1 is not None:
             assert self.relate_bsp1.klu.idx == relate_bsp1.klu.idx
+
+    def add_feat(self, inp1: Union[str, Dict[str, float], Dict[str, Optional[float]], 'CFeatures'], inp2: Optional[float] = None):
+        self.features.add_feat(inp1, inp2)
---
+++ ChanModel/Features.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+
+class CFeatures:
+    def __init__(self, initFeat=None):
+        self.__features = {} if initFeat is None else dict(initFeat)
+
+    def toJson(self):
+        return self.__features
+
+    def __getitem__(self, k):
+        return self.__features[k]
+
+    def add_feat(self, inp1, inp2: Optional[float] = None):
+        if inp2 is None:
+            self.__features.update(inp1)
+        else:
+            self.__features.update({inp1: inp2})
