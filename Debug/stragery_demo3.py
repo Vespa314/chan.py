47960b21eb0043f69cdbc7614c5830401436d29b
Vespa314 <kylensherlock@gmail.com>
2023-09-16 13:41:41 +0800

    fix: 更新demo3

--- Debug/stragery_demo3.py
+++ Debug/stragery_demo3.py
@@ -1,4 +1,4 @@
-from copy import copy
+import copy
 from typing import List
 
 from Chan import CChan
@@ -8,14 +8,14 @@ from DataAPI.BaoStockAPI import CBaoStock
 from KLine.KLine_Unit import CKLine_Unit
 
 
-def combine_5m_klu_form_1m(klu_1m_lst: List[CKLine_Unit]) -> CKLine_Unit:
+def combine_60m_klu_form_15m(klu_15m_lst: List[CKLine_Unit]) -> CKLine_Unit:
     return CKLine_Unit(
         {
-            DATA_FIELD.FIELD_TIME: klu_1m_lst[-1].time,
-            DATA_FIELD.FIELD_OPEN: klu_1m_lst[0].open,
-            DATA_FIELD.FIELD_CLOSE: klu_1m_lst[-1].close,
-            DATA_FIELD.FIELD_HIGH: max(klu.high for klu in klu_1m_lst),
-            DATA_FIELD.FIELD_LOW: min(klu.low for klu in klu_1m_lst),
+            DATA_FIELD.FIELD_TIME: klu_15m_lst[-1].time,
+            DATA_FIELD.FIELD_OPEN: klu_15m_lst[0].open,
+            DATA_FIELD.FIELD_CLOSE: klu_15m_lst[-1].close,
+            DATA_FIELD.FIELD_HIGH: max(klu.high for klu in klu_15m_lst),
+            DATA_FIELD.FIELD_LOW: min(klu.low for klu in klu_15m_lst),
         }
     )
 
@@ -25,10 +25,10 @@ if __name__ == "__main__":
     代码不能直接跑，仅用于展示如何实现小级别K线更新直接刷新CChan结果
     """
     code = "sz.000001"
-    begin_time = "2021-01-01"
+    begin_time = "2023-09-10"
     end_time = None
     data_src = DATA_SRC.BAO_STOCK
-    lv_list = [KL_TYPE.K_5M, KL_TYPE.K_1M]
+    lv_list = [KL_TYPE.K_60M, KL_TYPE.K_15M]
 
     config = CChanConfig({
         "triger_step": True,
@@ -42,31 +42,36 @@ if __name__ == "__main__":
         config=config,
     )
     CBaoStock.do_init()
-    data_src = CBaoStock(code, k_type=KL_TYPE.K_1M, begin_date=begin_time, end_date=end_time, autype=AUTYPE.QFQ)  # 获取最小级别
+    data_src = CBaoStock(code, k_type=KL_TYPE.K_15M, begin_date=begin_time, end_date=end_time, autype=AUTYPE.QFQ)  # 获取最小级别
 
-    klu_1m_lst_tmp: List[CKLine_Unit] = []  # 存储用于合成当前5M K线的1M k线
+    klu_15m_lst_tmp: List[CKLine_Unit] = []  # 存储用于合成当前60M K线的15M k线
 
-    for klu_1m in data_src.get_kl_data():  # 获取单根1分钟K线
-        klu_1m_lst_tmp.append(klu_1m)
-        klu_5m = combine_5m_klu_form_1m(klu_1m_lst_tmp)  # 合成5分钟K线
+    for klu_15m in data_src.get_kl_data():  # 获取单根15分钟K线
+        klu_15m_lst_tmp.append(klu_15m)
+        klu_60m = combine_60m_klu_form_15m(klu_15m_lst_tmp)  # 合成60分钟K线
 
         """
         拷贝一份chan_snapshot
         如果是用序列化方式，这里可以采用pickle.load()
         """
         chan: CChan = copy.deepcopy(chan_snapshot)
-        chan.trigger_load({KL_TYPE.K_5M: [klu_5m], KL_TYPE.K_1M: klu_1m_lst_tmp})
+        chan.trigger_load({KL_TYPE.K_60M: [klu_60m], KL_TYPE.K_15M: klu_15m_lst_tmp})
 
         """
+        策略开始：
         这里基于chan实现你的策略
         """
+        for kl_type, ele_manager in chan.kl_datas.items():
+            # 打印当前每一级别分别有多少K线
+            print(klu_15m.time, kl_type, sum(len(klc) for klc in ele_manager))
+        # 策略结束：
 
-        if len(klu_1m_lst_tmp) == 5:  # 已经完成5根1分钟K线了，说明这个最新的5分钟K线和里面的5根1分钟K线在将来不会再变化
+        if len(klu_15m_lst_tmp) == 4:  # 已经完成4根15分钟K线了，说明这个最新的60分钟K线和里面的4根15分钟K线在将来不会再变化
             """
             把当前完整chan重新保存成chan_snapshot
             如果是序列化方式，这里可以采用pickle.dump()
             """
             chan_snapshot = chan
-            klu_1m_lst_tmp = []  # 清空1分钟K线，用于下一个五分钟周期的合成
+            klu_15m_lst_tmp = []  # 清空1分钟K线，用于下一个五分钟周期的合成
 
     CBaoStock.do_close()
