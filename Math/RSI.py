a594c9953bbdc40ed5ffbf48b834e3c28f546597
Vespa314 <kylensherlock@gmail.com>
2025-04-28 17:58:50 +0800

    fix: rsi calculation

--- Math/RSI.py
+++ Math/RSI.py
@@ -11,10 +11,14 @@ class RSI:
         self.close_arr.append(close)
         if len(self.close_arr) == 1:
             return 50.0
+
         self.diff.append(self.close_arr[-1] - self.close_arr[-2])
+
         if len(self.diff) < self.period:
-            self.up.append(sum(x for x in self.diff if x > 0)/self.period)
-            self.down.append(sum(-x for x in self.diff if x < 0)/self.period)
+            up_sum = sum(x for x in self.diff if x > 0)
+            down_sum = sum(-x for x in self.diff if x < 0)
+            self.up.append(up_sum / len(self.diff))
+            self.down.append(down_sum / len(self.diff))
         else:
             if self.diff[-1] > 0:
                 upval = self.diff[-1]
@@ -22,8 +26,13 @@ class RSI:
             else:
                 upval = 0.0
                 downval = -self.diff[-1]
+
             self.up.append((self.up[-1] * (self.period - 1) + upval) / self.period)
             self.down.append((self.down[-1] * (self.period - 1) + downval) / self.period)
-        rs = self.up[-1] / self.down[-1] if self.down[-1] != 0 else 0
+
+        if self.down[-1] == 0:
+            return 100.0 if self.up[-1] > 0 else 0.0
+
+        rs = self.up[-1] / self.down[-1]
         rsi = 100.0 - 100.0 / (1.0 + rs)
         return rsi
