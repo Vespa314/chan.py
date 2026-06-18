f7749656e0290bb192d6e7a3c66b912f62011145
Vespa314 <kylensherlock@gmail.com>
2026-06-18 16:06:35 +0800

    fix: seg bug

--- Seg/SegListComm.py
+++ Seg/SegListComm.py
@@ -114,6 +114,9 @@ class CSegListComm(Generic[SUB_LINE_TYPE]):
             self.collect_first_seg(bi_lst)
         else:
             self.collect_segs(bi_lst)
+            if bi_lst[-1].idx - self.lst[-1].bi_list[-1].idx > 2 and not self.lst[-1].is_sure:
+                self.lst = self.lst[:-1]
+                self.collect_segs(bi_lst)
 
     def collect_left_as_seg(self, bi_lst: CBiList):
         last_bi = bi_lst[-1]
